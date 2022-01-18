4931b503833bde0046e0cd6a0cb1b9817c99a93e
J <info_together@aliyun.com>
2022-01-18 17:30:32 +0800

    chore(ts): Add TypeScript support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // Test import of a JavaScript module
-import { example } from '@/js/example'
+import { example } from '@/assets/js/example'
+
+import {APP_NAME} from '@/version'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+import webpackLogo from '@/assets/images/webpack-logo.svg'
 
 // Test import of styles
-import '@/styles/index.scss'
+import '@/assets/styles/index.scss'
 
 // Appending to the DOM
 const logo = document.createElement('img')
@@ -14,13 +16,18 @@ logo.src = webpackLogo
 const heading = document.createElement('h1')
 heading.textContent = example()
 
+
 // Test a background image url in CSS
 const imageBackground = document.createElement('div')
 imageBackground.classList.add('image')
 
 // Test a public folder asset
 const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+imagePublic.src = '@/assets/images/example.png'
+
+const ts = document.createElement('div');
+ts.textContent = APP_NAME;
+
 
 const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+app.append(logo, heading, imageBackground, imagePublic, ts);
---
+++ src/version.ts
@@ -0,0 +1,5 @@
+import { version, name } from '../package.json';
+
+export const APP_VERSION = version;
+
+export const APP_NAME = name;
